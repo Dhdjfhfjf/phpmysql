46ce9463843225e5773a6990e81a1364c587c844
Dora <da314159260818@gmail.com>
2025-03-10 14:39:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,29 @@
 </head>
 <body>
     <h1>活動報名表</h1>
+   <form action="">
+
+   <fieldset>
+    <legend>基本資料</legend>
+    <p>
+        <label for="name">姓名</label>
+        <input type="text" name="name" id="name" value="" placeholder="請用中文" required>
+    </p>
+   </fieldset>
+
+   <fieldset>
+    <legend>使用行為</legend>
+   </fieldset>
+
+   <fieldset>
+    <legend>滿意度</legend>
+   </fieldset>
+
+   <fieldset>
+    <legend>資料上傳</legend>
+   </fieldset>
+
+    <input type="submit" value="送出">
+   </form>
 </body>
 </html>
